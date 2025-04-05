0b969f0623ac755cfc8e43c3dcb2215c11d6a44a
abdulaziz omonov <omonov2006omonov@gmail.com>
2025-04-06 00:22:45 +0500

    service interface created

--- internal/service/chat_service.go
+++ internal/service/chat_service.go
@@ -1,1 +1,24 @@
 package service
+
+import (
+	"chat-app/internal/model"
+	"chat-app/internal/repository"
+	"time"
+)
+
+type ChatService interface {
+	RegisterUser(username, password string) (*model.User, error)
+	BlockUser(blockerID, blockedID uint) error
+
+	CreateGroup(name *string, memberIDs []uint) (*model.Group, error)
+	SendMessage(senderID uint, groupID *uint, content string, scheduledAt *time.Time) error
+	GetGroupMessages(groupID uint) ([]model.Message, error)
+}
+
+type chatService struct {
+	repo repository.ChatRepository
+}
+
+func NewChatService(repo repository.ChatRepository) ChatService {
+	return &chatService{repo: repo}
+}
