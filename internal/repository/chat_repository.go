1ce5ecca4ed99f77b30b9b3cb5bb8d91a88944b8
abdulaziz omonov <omonov2006omonov@gmail.com>
2025-04-06 00:19:18 +0500

    estimated interface created

--- internal/repository/chat_repository.go
+++ internal/repository/chat_repository.go
@@ -1,1 +1,31 @@
 package repository
+
+import (
+	"chat-app/internal/model"
+	"gorm.io/gorm"
+	"time"
+)
+
+type ChatRepository interface {
+	CreateUser(user *model.User) error
+	GetUserByID(id uint) (*model.User, error)
+	GetUserByUsername(username string) (*model.User, error)
+	BlockUser(blocked *model.BlockedUser) error
+	IsBlocked(senderID, receiverID uint) (bool, error)
+
+	CreateGroup(group *model.Group) error
+	AddUserToGroup(groupID, userID uint) error
+	GetGroupMembers(groupID uint) ([]model.User, error)
+
+	CreateMessage(msg *model.Message) error
+	GetMessagesByGroup(groupID uint) ([]model.Message, error)
+	GetScheduledMessages() ([]model.Message, error)
+}
+
+type chatRepository struct {
+	db *gorm.DB
+}
+
+func NewChatRepository(db *gorm.DB) ChatRepository {
+	return &chatRepository{db: db}
+}
